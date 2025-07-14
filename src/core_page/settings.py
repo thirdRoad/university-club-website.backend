718cae03c1306d1040978c1b298ce0c141b224d7
denigenc <deniz.genc@octoxlabs.com>
2025-07-14 13:43:12 +0300

    add: basic views, url orantation, postgres

--- src/core_page/settings.py
+++ src/core_page/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'core_page.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "veritabani_adi",
+        'USER': "kullanici_adi",
+        'PASSWORD': "sifre",
+        'HOST': "postgres",
+        'PORT': "5432",
     }
 }
 
