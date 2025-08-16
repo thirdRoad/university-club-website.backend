5486243443d9eb1a0a9a258119eec7ad0fddbb7b
denizugurgenc02 <deniz.genc@octoxlabs.com>
2025-08-17 00:06:41 +0300

    refactor: dockerfile, django app

--- src/core_page/views.py
+++ src/core_page/views.py
@@ -1,10 +1,17 @@
-from django.http import HttpResponse,JsonResponse
+import datetime
+import logging
+
+from django.http import JsonResponse
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
-    data = {
-    "name": "eren",
-    "user": "admin"
-    }
-    return JsonResponse(data)
-
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return JsonResponse(
+        {
+            "name": "eren",
+            "user": "admin",
+            "server_time": current_date,
+        }
+    )
