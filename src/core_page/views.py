7588e60dce6e97bfbbd7077de4972f66d5e698ea
figen arslan <figenarsss@gmail.com>
2025-08-17 23:18:05 +0300

    refactor:change name eren to figen int views.py

--- src/core_page/views.py
+++ src/core_page/views.py
@@ -7,7 +7,7 @@ def index(request):
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return JsonResponse(
         {
-            "name": "eren",
+            "name": "figen",
             "user": "admin",
             "server_time": current_date,
         }
