b1869c07733a8cd572997bbb699e7c1476f7f2ab
figen arslan <figenarsss@gmail.com>
2025-08-20 07:10:39 +0300

    Initial commit

--- src/core/settings.py
+++ src/core/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
-
 
 
 # Password validation
