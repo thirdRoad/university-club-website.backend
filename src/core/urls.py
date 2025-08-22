1d907abbe31fc2ee413b3b3471bbfc7ef349b0f3
denizugurgenc02 <denizugurgenc02@hotmail.com>
2025-08-22 22:05:17 +0300

    refactor: main app page core_page to core. delete: views.py body. #TASK-2

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for core_page project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.index, name="index"),
+    path("admin/", admin.site.urls),
 ]
